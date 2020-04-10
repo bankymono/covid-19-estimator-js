6ff3bfc5d202cf912ecbb6b3d32e4f83aa274b91
Bankole Ayodeji <bankymono@gmail.com>
2020-04-10 18:56:56 +0100

    Built currentlyInfected function to compute values impact and severe impact output

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,32 @@
-const covid19ImpactEstimator = (data) => data;
+import currentlyInfected from './utility_functions/currentlyInfected';
+
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  
+  const currInfected = currentlyInfected(input.reportedCases, 10);
+  const severeCurrInfected = currentlyInfected(input.reportedCases, 50);
+
+  return {
+    data: input,
+    impact: {
+      currentlyInfected: currInfected,
+      infectionsByRequestedTime: 0,
+      severeCasesByRequestedTime: 0,
+      hospitalBedsByRequestedTime: 0,
+      casesForICUByRequestedTime: 0,
+      casesForVentilatorsByRequestedTime: 0,
+      dollarsInFlight: 0
+    },
+    severeImpact: {
+      currentlyInfected: severeCurrInfected,
+      infectionsByRequestedTime: 0,
+      severeCasesByRequestedTime: 0,
+      hospitalBedsByRequestedTime: 0,
+      casesForICUByRequestedTime: 0,
+      casesForVentilatorsByRequestedTime: 0,
+      dollarsInFlight: 0
+    }
+  }
+};
 
 export default covid19ImpactEstimator;
