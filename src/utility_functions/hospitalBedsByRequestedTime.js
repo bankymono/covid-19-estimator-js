aaff3073785c4074fe8b2bc3e6c9130c4ada45b9
Bankole Ayodeji <bankymono@gmail.com>
2020-04-11 00:00:03 +0100

    7th rectification to  fix bug

--- src/utility_functions/hospitalBedsByRequestedTime.js
+++ src/utility_functions/hospitalBedsByRequestedTime.js
@@ -2,9 +2,9 @@ const availableBeds = (severeCases, hospitalBeds) => {
   const percent = 35 / 100;
   const bedsAvail = hospitalBeds * percent;
   const truncBedsAvail = Math.trunc(bedsAvail);
-  const notAvailForPatients = severeCases - truncBedsAvail;
-  if (notAvailForPatients > 0) {
-    return -notAvailForPatients;
+  const availForPatients = truncBedsAvail - severeCases;
+  if (availForPatients < 0) {
+    return availForPatients;
   }
   return truncBedsAvail;
   //
