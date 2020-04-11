23f1a532363af3ee081edeba7e668e3acf971bc5
Bankole Ayodeji <bankymono@gmail.com>
2020-04-11 10:28:45 +0100

    15th rectification

--- src/utility_functions/hospitalBedsByRequestedTime.js
+++ src/utility_functions/hospitalBedsByRequestedTime.js
@@ -2,9 +2,7 @@ const availableBeds = (severeCases, hospitalBeds) => {
   const percent = 35 / 100;
   const bedsAvail = hospitalBeds * percent;
   const availForPatients = bedsAvail - severeCases;
-  if (availForPatients < 0) {
-    return availForPatients;
-  }
+  //
   return availForPatients;
   //
 };
